cecd049d81347c4962a8d00ce4135da0e11bc7df
othman31 <othman0031@gmail.com>
2022-08-07 15:22:46 +0100

    adding animation

--- app.js
+++ app.js
@@ -9,10 +9,16 @@ function animateSlides() {
   const nav = document.querySelector(".nav-header");
 
   slides.forEach((slide) => {
-    console.log(slide);
     const revealImg = slide.querySelector(".reveal-img");
     const img = slide.querySelector("img");
     const revealText = slide.querySelector(".reveal-text");
+    //GSAP
+    const slideTl = gsap.timeline({
+      default: { duration: 5, ease: "power2.inOut" },
+    });
+    slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" });
+    slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
+    //-=1 so that it animate at the sam time with the revealImg
   });
 }
 
